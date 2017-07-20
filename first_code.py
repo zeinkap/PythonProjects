df8594079beeee01eb579305399d9f9a09e2098d
Zein <zeinkap@gmail.com>
2017-07-19 20:03:14 -0400

    Add files via upload

---
+++ first_code.py
@@ -0,0 +1,20 @@
+
+t = int(input("What is the temperature outside?"))
+if t > 60 and t < 90:
+    print("warm")
+elif t >= 90:
+    print("hot")
+elif t < 60:
+    print("chilly")
+else:
+    print("please try again")
+
+
+
+
+
+
+
+
+
+
