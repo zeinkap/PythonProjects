66c168b7cc2dc454f5ca250638f47b52892ad47a
Zein <zeinkap@gmail.com>
2017-07-25 20:21:45 -0400

    assignment 1 from ppt 8.5

---
+++ assignment1.py
@@ -0,0 +1,28 @@
+#Assignment 1
+def temp():
+    fahrenheit= int(input("Enter temperature in fahrenheit: "))
+    celsius= (fahrenheit-32) * 5/9
+    print("In celsius, the temperature is:",celsius,"°C")
+    print("In fahrenheit, the temperature was:",fahrenheit,"°F")
+temp()
+z = input("Do you want to end the program?")
+while z != 'no':
+    temp()
+print("Have a good day!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
